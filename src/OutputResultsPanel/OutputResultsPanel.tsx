37f3115d40e153ebea1190491bea79be94122649
Yulian Ovdey <yulianovdey@gmail.com>
2020-08-15 21:55:56 -0400

    handle negative score change

--- src/OutputResultsPanel/OutputResultsPanel.tsx
+++ src/OutputResultsPanel/OutputResultsPanel.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import styles from './OutputResultsPanel.module.css';
 import SearchResult from 'SearchResult';
 import ScoreSet from 'ScoreSet';
@@ -7,6 +7,13 @@ type props = {
   results: SearchResult[],
   scoreSet: ScoreSet,
 };
+
+function ScoreDifference(props: { a: number, b: number }) {
+  const difference = Math.abs(props.a - props.b);
+  const sign = props.b < props.a ? '-' : '+';
+
+  return <Fragment>{sign} {difference}</Fragment>
+}
 
 function OutputResultsPanel(props: props) {
   function compare(a: SearchResult, b: SearchResult) {
@@ -19,7 +26,7 @@ function OutputResultsPanel(props: props) {
     <div className={styles.container}>
       {results.map((result) => (
         <div className={styles.result} key={result.id}>
-          <span className={styles.score}>{props.scoreSet[result.id]} (+ {props.scoreSet[result.id] - result.score})</span>
+          <span className={styles.score}>{props.scoreSet[result.id]} (<ScoreDifference a={result.score} b={props.scoreSet[result.id]} />)</span>
           <pre>{JSON.stringify(result.source, null, 4)}</pre>
         </div>
       ))}
